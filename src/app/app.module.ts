e396e7bc4cb0058ae45d70bb952ef7d3302f9ff9
Ignacio Damián González <ignaciodamiang@gmail.com>
2022-10-23 22:36:42 -0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MarketingComponent } from './components/marketing/marketing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    FeaturesComponent,
+    PricingComponent,
+    CarouselComponent,
+    FooterComponent,
+    MarketingComponent
   ],
   imports: [
     BrowserModule,
